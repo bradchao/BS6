24b0b401df3f1d3f990c692a86a7c3255352df2a
User <User@10.0.100.157>
2024-09-23 09:19:41 +0800

    09230919

---
+++ BS6/src/main/java/tw/brad/Bs6Application.java
@@ -0,0 +1,13 @@
+package tw.brad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bs6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bs6Application.class, args);
+	}
+
+}
---
+++ BS6/src/test/java/tw/brad/Bs6ApplicationTests.java
@@ -0,0 +1,13 @@
+package tw.brad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bs6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
