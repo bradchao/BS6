8b8893d964aef327f15809e627e7120c594f54c5
User <User@10.0.100.157>
2024-09-24 16:27:45 +0800

    09241627

--- BS6/src/main/java/tw/brad/controller/ViewController.java
+++ BS6/src/main/java/tw/brad/controller/ViewController.java
@@ -1,14 +1,23 @@
 package tw.brad.controller;
 
+import java.util.LinkedList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import tw.brad.service.CustomersServiceImpl;
+
 @Controller
 public class ViewController {
 	ModelAndView modelBrad01 = new ModelAndView("brad01");
 	ModelAndView modelBrad02 = new ModelAndView("brad02");
+	
+	@Autowired
+	private CustomersServiceImpl customersService;
 	
 	public ViewController() {
 		modelBrad01.addObject("mesg", "Hello, Brad01");
@@ -30,6 +39,21 @@ public class ViewController {
 	@RequestMapping("/brad02")
 	public ModelAndView brad02() {
 		return modelBrad02;
+	}
+	
+	@RequestMapping("/brad03")
+	public String brad03(Model model) {
+		model.addAttribute("companyName", "Brad Big Company");
+		
+		//List<Customers> customers = customersService.getAll();
+		List<String> names = new LinkedList<String>();
+		names.add("Test1");
+		names.add("Test2");
+		names.add("Test3");
+		
+		model.addAttribute("names", names);
+		
+		return "brad03";
 	}
 	
 }
--- BS6/src/main/java/tw/brad/repository/CustRepo.java
+++ BS6/src/main/java/tw/brad/repository/CustRepo.java
@@ -16,8 +16,11 @@ public interface CustRepo extends JpaRepository<Customers, String> {
 	@Query("SELECT c FROM Customers c")
 	List<Customers> findAllCustomers();
 	
-	@Query("SELECT c.companyName FROM Customers c WHERE c.customerId = :id")
-	String findCustomerId(@Param("id") String customerID);
+//	@Query("SELECT c.companyName FROM Customers c WHERE c.customerId = :id")
+//	String findCustomerId(@Param("id") String customerID);
+
+	@Query("SELECT 'Hello'")
+	String findCustomerId(String customerID);
 	
 	@Query("SELECT new tw.brad.model.Cust(c.companyName, c.city, c.country) FROM Customers c WHERE c.customerId = :id")
 	Cust findCustomerIdV2(@Param("id") String customerID);
