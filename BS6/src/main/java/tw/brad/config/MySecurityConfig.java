4be09b4507bef76d66c02d2139d89d50267c8089
User <User@10.0.100.157>
2024-09-25 10:38:55 +0800

    09251038

--- BS6/src/main/java/tw/brad/config/MySecurityConfig.java
+++ BS6/src/main/java/tw/brad/config/MySecurityConfig.java
@@ -2,7 +2,14 @@ package tw.brad.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.auditing.config.AuditingConfiguration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -28,5 +35,31 @@ public class MySecurityConfig {
 		
 		return httpSecurity.build();
 	}
+	@Bean
+	public UserDetailsService createUser() {
+		UserDetails user1 = 
+				User.withDefaultPasswordEncoder()
+				.username("brad")
+				.password("123456")
+				.roles("USER")
+				.build();
+		UserDetails user2 = 
+				User.withDefaultPasswordEncoder()
+				.username("ispan")
+				.password("123456")
+				.roles("USER")
+				.build();
+		
+		InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager(user1, user2);
+		return manager;
+	}
+	
+	@Bean
+	public AuthenticationManager createManager(AuthenticationConfiguration conf) throws Exception {
+		return conf.getAuthenticationManager();
+	}
+	
+	
+	
 	
 }
